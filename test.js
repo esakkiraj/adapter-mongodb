6f38d578911d1e171b65767645fc419805146933
esakkiraj.a <esakkiraj.a@zohocorp.com>
2017-05-12 16:59:18 +0530

    bumped version to 0.0.5.

--- test.js
+++ test.js
@@ -2,22 +2,27 @@ const MongoClient = require('mongodb').MongoClient;
 const test = require('micro-analytics-cli/adapter-tests/unit-tests');
 const path = require('path');
 
-const URL = `mongodb://localhost:27017/nalyticsdb`;
+const URL = `mongodb://localhost:27017/anlyticsdb`;
 
-MongoClient
-  .connect(URL)
-  .then(function(connection) {
-    const db = connection;
+async function runTest() {
 
-    test({
-      name: 'mongodb',
-      modulePath: path.resolve(__dirname, './index.js'),
-      afterAll: async (adapter) => {
-        await db.close()
-      }
+  var Clinent = MongoClient .connect(URL)
+    .then(function(connection) {
+      const db = connection;
+
+      test({
+        name: 'mongodb',
+        modulePath: path.resolve(__dirname, './index.js'),
+        afterAll: async (adapter) => {
+          await db.close()
+        }
+      })
+
     })
+    .catch(function(err) {
+      console.log(err);
+    });
 
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
+}
+
+runTest();
